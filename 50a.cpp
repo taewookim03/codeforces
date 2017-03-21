a004f70871291dfdb6f5aed185d076f1693a8919
Taewoo Kim <taewookim03@gmail.com>
2017-03-21 18:25:22 -0500

    Added single-compilation plugin and make file

---
+++ 50a.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Taewoo Kim on 2/7/2017.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <regex>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class Base {
+    virtual void method() {std::cout << "from Base" << std::endl;}
+public:
+    virtual ~Base() {method();}
+    void baseMethod() {method();}
+};
+
+class A : public Base {
+    void method() {std::cout << "from A" << std::endl;}
+public:
+    ~A() {method();}
+};
+
+void f(int arr[]){
+    sort(arr, arr + 5);
+}
+
+int main(void) {
+
+    string s = "(2, 2) (3, 3)";
+    string t = regex_replace(s, regex("[\\(\\),]"), " ");
+    cout << t;
     return 0;
 }
